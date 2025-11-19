1023fef689008908bc206b62f93d04ca8e7de4e3
cmalongo <malongo.cyrille@gmail.com>
2025-11-19 17:06:04 +0100

    Suppresion de modification test

--- main.py
+++ main.py
@@ -426,8 +426,7 @@ def main():
     # You can later use `best_history` in a notebook to plot:
     # - best accuracy vs generation
     # - best pruning_rate vs generation
-    # test
-
+    
 
 if __name__ == "__main__":
     main()
